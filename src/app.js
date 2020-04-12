26b052094d1aaef212bd2ab99aef64715e3bb454
Prajwal Nayak <prajwalnayak@Prajwals-MacBook-Pro.local>
2020-04-12 17:35:30 +0800

    pushing all changes

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! Prajwal Nayak</h1>
+        <h1> Hello, World! Prajwal Nayak Test 4 </h1>
       </div>
     );
   }
